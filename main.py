e1031fa2e9f96fbb69547a7a8017ffc5fe03e3e1
Moritz Makowski <moritz.makowski@tum.de>
2022-06-12 19:26:05 +0000

    Use the filename from parser output

--- main.py
+++ main.py
@@ -20,26 +20,31 @@ def run(ifg_directory: str):
     # run the parser
     execution_output = subprocess.check_output(
         ["./ifg_parser", "ifg_parser.inp"], cwd=f"{PROJECT_DIR}/parser"
-    )
-    file_parsing_block = execution_output.decode().split("Done!")[-1]
+    ).decode()
+    with open(f"{PROJECT_DIR}/parser/output.txt", "w") as f:
+        f.write(execution_output)
+
+    file_parsing_block = execution_output.split("Done!")[-1]
     file_parsing_lines = file_parsing_block.split("Read OPUS parms:")[1:]
     results = {}
 
     # get results from output stream
     for line in file_parsing_lines:
         n = int(line[:12].replace(" ", ""))
+        filename = line[12:].split("\n")[0].split("/")[-1].replace(")", "")
         parser_output = line[12:].replace("\n", " ")
         parser_messages = re.findall('charfilter "[^"]+" is missing', parser_output)
         if len(parser_messages) > 0:
-            ifg_name = ifgs[n - 1].split("/")[-1]
-            results[ifg_name] = [x.split('"')[1] for x in parser_messages]
+            results[filename] = [x.split('"')[1] for x in parser_messages]
 
     return results
 
 
 if __name__ == "__main__":
     # example usage
-    results = run("/home/esm/em27_ifg_dss/proffast-archive/mb/ifgs/20220421")
+    results = run(
+        "/home/esm/automation-pipelines/automated-proffast-pylot/inputs/mb_ifg/220601"
+    )
 
     if len(results) > 0:
         print("The corrupt ifgs are: ")
